5bb7a2a6c15c09e50af87cfb887355c6733c023d
Zhdaniuk Olena <e.mssixty@gmail.com>
2018-02-05 09:11:04 +0200

    second iteration

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 let arr = [];
 
 
@@ -15,7 +14,7 @@ addKeyboardLayout('zxcvbnm,./');
 const keyboard = {
     layouts: {
         en: {
-            topRow: ['1sdfasdf'],
+            topRow: [],
             middleRow: [],
             bottomRow: []
         },
@@ -58,8 +57,6 @@ do {
         isCancelButtonPressed = true;
         break;
     }
-
-    question = +question;
     
     if( isQuestionCorrect(question) ) {
         isCorrectLangEntered = true;
@@ -67,13 +64,14 @@ do {
     
 } while(!isCorrectLangEntered);
 
+
 if (!isCancelButtonPressed) {    
     addCurrentLang();
 }
 
 
 function isQuestionCorrect() {
-    if(question >= 0 && question <= 2) {
+    if(question === '0' || question === '1' || question === '2' || false) {
         return true;
     } 
     else {
@@ -84,16 +82,13 @@ function isQuestionCorrect() {
 
 
 function addCurrentLang() {
-    if(question === 0) {
+    if(question === '0') {
         keyboard.currentLang = 'en';
-    } else if(question === 1) {
+    } else if(question === '1') {
         keyboard.currentLang = 'ru';
-    }  else if(question === 2) {
+    }  else if(question === '2') {
         keyboard.currentLang = 'ua';
-    } else if(question > 2 && question < 0) {
-        alert('This language does not exist. Please, try again');
-        return alert(question);
-    } else(question === null)
+    } else
         return;
 };
 
